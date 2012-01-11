8d375092f6a7f86b8d91450fb6fb194b0af9cf5a
clanrbr <clan_rbr@yahoo.com>
2012-01-11 23:44:20 +0200

    Step2 and Step3

---
+++ application/views/registration/registration_step2_view.php
@@ -0,0 +1,5 @@
+<?php include ('registration_steps.php'); ?>
+<form action="/register/step2" method="post">
+     <input type="textarea" name="ckeditor" />
+     <input type="submit" value="Запази" />
+</form>
---
+++ application/views/registration/registration_step3_view.php
@@ -0,0 +1,6 @@
+<?php include ('registration_steps.php'); ?>
+<form action="/register/step3" method="post">
+     <input type="textarea" name="ckeditor" />
+     <input type="submit" value="Нова Страница" name="new" />
+     <input type="button" value="Приключих" id="toregister" />
+</form>
