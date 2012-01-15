08124e4b27c7339f3a789a071df9286e4ccc2373
V <vcrazy@abv.bg>
2012-01-15 18:24:06 +0200

    ..

--- application/models/model_register.php
+++ application/models/model_register.php
@@ -3,33 +3,33 @@
 class Model_register extends CI_Model {
 
     public function save_registration() {
-        $this->load->library('session');
-        $session_id = $this->session->userdata('session_id');
-        $this->session->userdata('nextpage');
-        $this->session->userdata('homepage');
-        $this->session->userdata('template');
-        $this->input->post('nextpage');
-        $this->input->post('homepage');
-        $this->input->post('template');
-        $nextpage = $this->input->post('nextpage');
-        $homepage = $this->input->post('homepage');
-        $template = $this->input->post('template');
-        $this->$email = $_POST['email'];
-        $this->$password = $_POST['password'];
-        $this->$domain = $_POST['domain'];
+//        $this->load->library('session');
+//        $session_id = $this->session->userdata('session_id');
+//        $this->session->userdata('nextpage');
+//        $this->session->userdata('homepage');
+//        $this->session->userdata('template');
+//        $this->input->post('nextpage');
+//        $this->input->post('homepage');
+//        $this->input->post('template');
+        $nextpage = $this->input->post('nextpage'); // from session
+        $homepage = $this->input->post('homepage'); // -||-
+        $template = $this->input->post('template'); // -||-
+        $this->email = $_POST['email'];
+        $this->password = $_POST['password'];
+        $this->domain = $_POST['domain'];
 
         $data = array(
-            'email' => '$email',
-            'password' => '$password',
-            'domain' => '$domain'
+            'email' => $this->db->escape($email),
+            'password' => md5($this->db->escape($password)),
+            'domain' => $this->db->escape($domain)
         );
         $this->db->insert('users', $data);
         $user_id = $this->db->insert_id();
         $site = array(
-            'site_name' => '$domain',
-            'user_id' => '$user_id',
-            'site_url' => '$domain' . 'exo.bg',
-            'template_id' => '$template'
+            'site_name' => '$domain', // db->escape
+            'user_id' => '$user_id', // -||-
+            'site_url' => '$domain', // -||-
+            'template_id' => '$template' // -||-
         );
         $this->db->insert('sites', $site);
         $site_id = $this->db->insert_id();
@@ -37,14 +37,16 @@ class Model_register extends CI_Model {
         foreach ($nextpage as $page1) {
             $count++;
             $page = array(
-            'page_num' => '$count',
-            'page_content' => '$page1',
-            'site_id' => '$site_id',
-            'user_id' => '$user_id'
+            'page_num' => '$count', // -||-
+            'page_content' => '$page1', // -||-
+            'site_id' => '$site_id', // -||-
+            'user_id' => '$user_id' // -||-
             );
                     
             $this->db->insert('pages', $page);
         }
+
+		// return?
     }
 
 }
