393193dc4cddc6340dad437e249df9f727dbb77e
Tsvetomir Shavely <my_hze@hotmail.com>
2012-01-15 16:04:22 +0200

    Header menu and css

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -5,13 +5,35 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link href="/css/reset.css" rel="stylesheet" type="text/css" />
 		<link href="/css/style.css" rel="stylesheet" type="text/css" />
-		<script src="/js/jquery.js" type="text/javascript"></script>
+		<link href="/css/exoHeader.css" rel="stylesheet" type="text/css" media="screen" />
+                <script src="/js/jquery.js" type="text/javascript"></script>
 		<script src="/js/script.js" type="text/javascript"></script>
+                
 	</head>
 	<body>
-		<div id="header">
-			<a href="/">
-				EXO header
-			</a>
-		</div>
+<div id="header-left">
+  <!-- Header - left -->
+  <div id="header-right">
+    <!-- header - right -->
+    <div id="header">
+      <!-- header -->
+      <div id="header-content">
+        <!-- header-content -->
+        <h1 id="logo"><a href="exoindex3.html" rel="index" title="Web Design Company">Web Design Company</a> <span title="MASS MEDIA GROUP LTD"></span> </h1>
+        <ul id="nav">
+          <!-- Navigation Bar -->
+          <li id="Link-Home"><a href="exoindex2.html">Na4alo<span class="wrap-bg"><span><!-- --></span></span></a></li>
+          <li id="Link-Services"><a href="exoindex3.html">Vhod<span class="wrap-bg"><span><!-- --></span></span></a></li>
+          <li id="Link-Portfolio"><a href="exoindex3.html">Syzdai dizain<span class="wrap-bg"><span><!-- --></span></span></a></li>
+          
+        </ul><div id="light" style="left: 10px; top: 0px; "></div>
+	  </div>
+      <!-- /header-content -->
+    </div>
+    <!-- /header -->
+  </div>
+  <!-- /header - right -->
+</div>
+<!-- /Header - left -->
+
 		<?php include('menu.php'); ?>
