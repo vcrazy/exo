f89f99065bf737565d382824ed0f71f90423fa5e
clanrbr <clan_rbr@yahoo.com>
2012-01-16 23:09:55 +0200

    Ckeditor fix

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -6,16 +6,9 @@
 		<link href="/css/reset.css" rel="stylesheet" type="text/css" />
 		<link href="/css/style.css" rel="stylesheet" type="text/css" />
 		<link href="/css/exoHeader.css" rel="stylesheet" type="text/css" media="screen" />
-<<<<<<< HEAD
                 <script src="/js/jquery.min.js" type="text/javascript"></script>
                 <script type="text/javascript" src="/js/ckeditor/ckeditor.js"></script>
                 <script type="text/javascript" src="/js/ckeditor/adapters/jquery.js"></script>
 		<script src="/js/script.js" type="text/javascript"></script>
-                
-=======
-		<script src="/js/jquery.min.js" type="text/javascript"></script>
-		<script src="/js/script.js" type="text/javascript"></script>
-		<script type="text/javascript" src="/js/ckeditor/ckeditor.js"></script>
->>>>>>> f9730a2ecd25e2786d5d77eeae308e3a0605177b
 	</head>
 	<body>
