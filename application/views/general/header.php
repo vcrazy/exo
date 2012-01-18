a04f2788268ed5b58e17df85e0affe58d693ebfe
Tsvetomir Shavely <my_hze@hotmail.com>
2012-01-18 22:39:19 +0200

    style materials add

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -6,18 +6,15 @@
 		<link href="/css/reset.css" rel="stylesheet" type="text/css" />
 		<link href="/css/style.css" rel="stylesheet" type="text/css" />
 		<link href="/css/exoHeader.css" rel="stylesheet" type="text/css" media="screen" />
-<<<<<<< HEAD
+
                 <link href="/css/exoFooter.css" rel="stylesheet" type="text/css" media="screen" />
                 <script src="/js/jquery.js" type="text/javascript"></script>
-=======
                 <script src="/js/jquery.min.js" type="text/javascript"></script>
                 <script type="text/javascript" src="/js/ckeditor/ckeditor.js"></script>
                 <script type="text/javascript" src="/js/ckeditor/adapters/jquery.js"></script>
->>>>>>> 53de03876ce2fd219419eb6721d6af91a93a42f3
 		<script src="/js/script.js" type="text/javascript"></script>
 	</head>
 	<body>
-<<<<<<< HEAD
 <div id="header-left">
   <!-- Header - left -->
   <div id="header-right">
@@ -29,9 +26,12 @@
         <h1 id="logo"><a href="exoindex3.html" rel="index" title="Web Design Company">Web Design Company</a> <span title="MASS MEDIA GROUP LTD"></span> </h1>
         <ul id="nav">
           <!-- Navigation Bar -->
-          <li id="Link-Home"><a class="button" href="exo.loc"><span>submit this shit</span></a></li>
-          <li id="Link-Services"><a class="button" href="http://google.com"><span>submit this shit</span></a></li>
-          <li id="Link-Portfolio"><a href="exoindex3.html"><span class="wrap-bg"><span><!-- --></span></span></a></li>
+          
+          <a class="button" href="http://google.com"><span>sdcbsd;</span></a>
+          <a class="button" href="http://google.com"><span>sdcbsd;</span></a>
+          <a class="button" href="http://google.com"><span>sdcasdasdasdasdasdasdbsd;</span></a>
+          <a class="button" href="http://google.com"><span>ssd;</span></a>
+
           
         </ul><div id="light" style="left: 10px; top: 0px; "></div>
 	  </div>
@@ -44,5 +44,4 @@
 <!-- /Header - left -->
 
 		<?php include('menu.php'); ?>
-=======
->>>>>>> 53de03876ce2fd219419eb6721d6af91a93a42f3
+
