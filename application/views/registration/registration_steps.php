8db0bb54c0328b0c78859385012946a40dd35c47
clanrbr <clan_rbr@yahoo.com>
2012-01-25 21:15:30 +0200

    steps links

--- application/views/registration/registration_steps.php
+++ application/views/registration/registration_steps.php
@@ -1,3 +1,4 @@
-СТЪПКА 1<br />
-СТЪПКА 2<br />
-СТЪПКА 3<br /><br />
+<a href="step1">СТЪПКА 1</a><br />
+<a href="step2">СТЪПКА 2</a><br />
+<a href="step3">СТЪПКА 3</a><br />
+<a href="step4">СТЪПКА 4</a><br />
