3beab9926fb60c4e07b60422b606a668284c7c87
V <vcrazy@abv.bg>
2012-02-15 22:17:57 +0200

    ..

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	$('div.page').not(':first').hide();
+
 	$('#menu li[id^=menu_] a').click(function(){
 		$('.page').hide();
 		$('#page_' + $(this).parent().attr('id')).show();
