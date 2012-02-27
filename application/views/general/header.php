35ebf9f4e5db3688d4d455b49825457f268930e6
V <vcrazy@abv.bg>
2012-02-27 22:01:16 +0200

    header ie fix

--- application/views/general/header.php
+++ application/views/general/header.php
@@ -18,67 +18,64 @@
 
 	</head>
 	<body>
-        <div id="toppanel">
-	  <div id="panel">
-            <div class="content clearfix">
-		<div class="left">
-                    <h1>Здравей!</h1>
-		</div>    
-	        <div class="left">
-		<!-- Login Form -->
-                    <form class="clearfix" action="" method="post">
-			<h1>Потребителски вход!</h1>
-					
-			<label class="grey" for="username">Имейл:</label>
-			<input class="field" type="text" name="email" id="email" value="" size="23" />
-			<label class="grey" for="password">Парола:</label>
-			<input class="field" type="password" name="password" id="password" size="23" />
-	           
-        		<div class="clear"></div>
-			<input type="submit" name="submit" value="Login" class="bt_login" />
-		    </form>
-		</div>
-		<div class="left right">			
-		<!-- Register Form -->
-		  <form action="" method="post">
-                    <h1>Регистрация:</h1>		
-                    <label class="grey" for="email">Имейл:</label>
-		    <input class="field" type="text" name="email" id="email" size="23" />
-		    <input type="submit" name="submit" value="Register" class="bt_register" />
-		  </form>
-		</div>
+		<div id="toppanel">
+			<div id="panel">
+				<div class="content clearfix">
+					<div class="left">
+						<h1>Здравей!</h1>
+					</div>    
+					<div class="left">
+					<!-- Login Form -->
+						<form class="clearfix" action="" method="post">
+							<h1>Потребителски вход!</h1>
 
-            
-<!--            <div class="left">
-            
-            <h1>Members panel</h1>
-            
-            <p>You can put member-only data here</p>
-            <a href="registered.php">View a special member page</a>
-            <p>- or -</p>
-            <a href="?logoff">Log off</a>
-            
-            </div>-->
-            
-                <div class="left right"></div>
-            </div>
-	  </div>
+							<label class="grey" for="username">Имейл:</label>
+							<input class="field" type="text" name="email" id="email" value="" size="23" />
+							<label class="grey" for="password">Парола:</label>
+							<input class="field" type="password" name="password" id="password" size="23" />
+
+							<div class="clear"></div>
+							<input type="submit" name="submit" value="Login" class="bt_login" />
+						</form>
+					</div>
+					<div class="left right">			
+					<!-- Register Form -->
+						<form action="" method="post">
+							<h1>Регистрация:</h1>		
+							<label class="grey" for="email">Имейл:</label>
+							<input class="field" type="text" name="email" id="email" size="23" />
+							<input type="submit" name="submit" value="Register" class="bt_register" />
+						</form>
+					</div>
+				<!--            <div class="left">
+
+				<h1>Members panel</h1>
+
+				<p>You can put member-only data here</p>
+				<a href="registered.php">View a special member page</a>
+				<p>- or -</p>
+				<a href="?logoff">Log off</a>
+
+				</div>-->
+
+					<div class="left right"></div>
+				</div>
+			</div>
             <!-- /login -->	
-	 <div class="tab">
-            <ul class="login">
-	       <li class="left">&nbsp;</li>
-	       <li>Здравей !</li>
-               <li class="sep">|</li>
-               <li id="toggle">
-                    <a id="open" class="open" href="#">Отвори панела</a>
-                    <a id="close" style="display: none;" class="close" href="#">Затвори панела</a>			
-	       </li>
-	       <li class="right">&nbsp;</li>
-	   </ul> 
-	</div> <!-- / top -->
-	
-       </div> <!--panel -->
-            
+			<div class="tab">
+			</div> <!-- / top -->
+				<ul class="login">
+					<li class="left">&nbsp;</li>
+					<li>Здравей !</li>
+					<li class="sep">|</li>
+					<li id="toggle">
+						<a id="open" class="open" href="#">Отвори панела</a>
+						<a id="close" style="display: none;" class="close" href="#">Затвори панела</a>			
+					</li>
+					<li class="right">&nbsp;</li>
+				</ul> 
+		</div> <!--panel -->
+
 		<div id="head">
 			<div id="head_cen">
 				<div id="head_sup" class="head_pad">
